40de507f29284fcb97cecc7644d177ea606e9d5c
Pablo400 <alu0101311135@ull.edu.es>
2022-04-30 20:23:07 +0000

    Commit inicial

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+/* eslint-disable require-jsdoc */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+exports.add = add;
+console.log(add(1, 2));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable require-jsdoc */
+
+export function add(firstNumber: number, secondNumber: number) {
+  return firstNumber + secondNumber;
+}
+
+console.log(add(1, 2));
