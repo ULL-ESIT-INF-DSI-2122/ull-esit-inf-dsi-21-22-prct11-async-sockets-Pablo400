e628dcadfc3fd08001b81f6db80aaed6dacfdacf
Pablo400 <alu0101311135@ull.edu.es>
2022-05-02 18:50:42 +0000

    .gitignore está trabajando

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* eslint-disable require-jsdoc */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
-exports.add = add;
-console.log(add(1, 2));
