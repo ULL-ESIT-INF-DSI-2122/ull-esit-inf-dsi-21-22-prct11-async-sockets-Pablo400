f56100760c3b70d30df98ec1e5cfe2aa955b80fb
Pablo400 <alu0101311135@ull.edu.es>
2022-05-05 11:53:20 +0000

    Prueba

--- src/NoteApp/NoteOperations/addNote.ts
+++ src/NoteApp/NoteOperations/addNote.ts
@@ -34,45 +34,45 @@ export class AddNote extends ChalkColor {
       if (err) {
         response = {type: 'add', success: false, error: color.getColor('red', 'Ese usuario no existe')};
         cb(response, undefined);
-      }
-
-      const json: any = {
-        title: title,
-        body: body,
-        color: noteColor,
-      };
-
-      if (title != '' && noteColor != '' && body != '' ) {
-        if (noteColor === 'red' || noteColor === 'green' || noteColor === 'yellow' || noteColor === 'blue') {
-          // Se comprueba si la nota ya existe
-          fs.access(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/${user}/${title}.json`, fs.constants.F_OK, (err: Error) => {
-            if (err) {
-              fs.writeFile(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/${user}/${title}.json`, JSON.stringify(json, null, 2), (err: Error) => {
-                if (err) {
-                  response = {type: 'add', success: false, error: color.getColor('red', 'No se ha podido crear la nota')};
-                  cb(response, undefined);
-                } else {
-                  response = {
-                    type: 'add',
-                    success: true,
-                    notes: [json],
-                  };
-                  cb(undefined, response);
-                }
-              });
-            } else {
-              response = {type: 'add', success: false, error: color.getColor('red', 'Esa nota ya existe')};
-              cb(undefined, response);
-            }
-          });
+      } else {
+        const json: any = {
+          title: title,
+          body: body,
+          color: noteColor,
+        };
+  
+        if (title != '' && noteColor != '' && body != '' ) {
+          if (noteColor === 'red' || noteColor === 'green' || noteColor === 'yellow' || noteColor === 'blue') {
+            // Se comprueba si la nota ya existe
+            fs.access(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/${user}/${title}.json`, fs.constants.F_OK, (err: Error) => {
+              if (err) {
+                fs.writeFile(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/${user}/${title}.json`, JSON.stringify(json, null, 2), (err: Error) => {
+                  if (err) {
+                    response = {type: 'add', success: false, error: color.getColor('red', 'No se ha podido crear la nota')};
+                    cb(response, undefined);
+                  } else {
+                    response = {
+                      type: 'add',
+                      success: true,
+                      notes: [json],
+                    };
+                    cb(undefined, response);
+                  }
+                });
+              } else {
+                response = {type: 'add', success: false, error: color.getColor('red', 'Esa nota ya existe')};
+                cb(undefined, response);
+              }
+            });
+          } else {
+            response = {type: 'add', success: false,
+              error: color.getColor('red', 'No se puede crear una nota si no se le indican un color, use: red, green, yellow o blue como colores')};
+            cb(response, undefined);
+          }
         } else {
-          response = {type: 'add', success: false,
-            error: color.getColor('red', 'No se puede crear una nota si no se le indican un color, use: red, green, yellow o blue como colores')};
+          response = {type: 'add', success: false, error: color.getColor('red', 'No se puede crear una nota vacía')};
           cb(response, undefined);
         }
-      } else {
-        response = {type: 'add', success: false, error: color.getColor('red', 'No se puede crear una nota vacía')};
-        cb(response, undefined);
       }
     });
   };
