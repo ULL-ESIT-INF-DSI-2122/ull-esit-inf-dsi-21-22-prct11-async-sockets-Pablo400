77cbf12906f9c962f13f011eebd20f18446f7e26
Pablo400 <alu0101311135@ull.edu.es>
2022-05-08 18:39:30 +0000

    Pruebas

--- tests/removeNote.spec.ts
+++ tests/removeNote.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import 'mocha';
 import {expect} from 'chai';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 import {RemoveNote} from '../src/NoteApp/NoteOperations/removeNote';
 import {ChalkColor} from '../src/NoteApp/NoteOperations/utilities';
@@ -24,23 +24,23 @@ describe('Remove Note Test', () => {
       }
     });
   });
-  it('Nota eliminada', (done) => {
-    const json = {
-      title: 'prueba2',
-      body: 'Prueba',
-      color: 'green',
-    };
-    fs.writeFileSync(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/pablo/prueba2.json`, JSON.stringify(json, null, 2));
-    removeNote.removeNoteCallback('pablo', 'prueba2', (_, data) => {
-      response = {
-        type: 'remove',
-        success: true,
-        notes: [json],
-      };
-      if (data) {
-        expect(data).to.be.eql(response);
-        done();
-      }
-    });
-  });
+  // it('Nota eliminada', (done) => {
+  //   const json = {
+  //     title: 'prueba2',
+  //     body: 'Prueba',
+  //     color: 'green',
+  //   };
+  //   fs.writeFileSync(`/home/usuario/ull-esit-inf-dsi-21-22-prct11-async-sockets-Pablo400/ProgramFiles/pablo/prueba2.json`, JSON.stringify(json, null, 2));
+  //   removeNote.removeNoteCallback('pablo', 'prueba2', (_, data) => {
+  //     response = {
+  //       type: 'remove',
+  //       success: true,
+  //       notes: [json],
+  //     };
+  //     if (data) {
+  //       expect(data).to.be.eql(response);
+  //       done();
+  //     }
+  //   });
+  // });
 });
